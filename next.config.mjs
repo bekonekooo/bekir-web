c4eee5272466f3849c78700b5eaaaca830bc394a
Berkay <berkayyaydin@gmail.com>
2025-08-27 13:34:06 +0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,23 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'images.unsplash.com',
-        
-//       },
-      
-//     ],
-//   },
-// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Statik yayınlayacaksan:
+  output: 'export',                 // npm run build → out/ üretir
+  images: {
+    unoptimized: true,              // static export için şart
+    domains: ['images.unsplash.com'] // dev’de unconfigured host hatasını keser
+  },
+};
 
+export default nextConfig;
 // export default nextConfig;
 
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-};
+// const nextConfig = {
+//   output: 'export',
+//   images: { unoptimized: true },
+// };
+
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   output: 'export',          // out/ üretir
+//   images: { unoptimized: true }, // export modunda next/image için şart
+// };
